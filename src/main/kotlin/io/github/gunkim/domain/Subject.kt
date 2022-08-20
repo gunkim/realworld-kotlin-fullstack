9a81b054b7d517602fdcb364698444ab34489a06
Pistol Kim <45007556+gunkim@users.noreply.github.com>
2022-08-20 16:23:42 +0900

    :sparkles: 과목 추가된 테스트 케이스 구현 (#4)
    * :white_check_mark: 과목 테스트 케이스 추가
    
    - 음수일 경우 인스턴스를 생성할 수 없음
    
    * :sparkles: 과목 도메인, 추가된 테스트 케이스 구현

--- src/main/kotlin/io/github/gunkim/domain/Subject.kt
+++ src/main/kotlin/io/github/gunkim/domain/Subject.kt
@@ -5,6 +5,11 @@ class Subject(
     val type: SubjectType,
     score: Int,
 ) {
+    init {
+        require(score >= 0) {
+            "점수는 음수가 될 수 없습니다. (score: ${score})"
+        }
+    }
     private var _score = score
     val score
         get() = _score
--- src/test/kotlin/io/github/gunkim/domain/SubjectTests.kt
+++ src/test/kotlin/io/github/gunkim/domain/SubjectTests.kt
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.assertThrows
 
 class SubjectTests {
     @Test
-    fun `점수를 변경한다`() {
-        val subject = Subject(type = SubjectType.Math, score = 12)
-        subject.changeScore(23)
-        assertThat(subject.score).isEqualTo(23)
+    fun `음수로는 인스턴스를 생성할 수 없다`() {
+        assertThrows<IllegalArgumentException>
+        { Subject(type = SubjectType.English, score = -1) }
+            .apply { assertThat(message).isEqualTo("점수는 음수가 될 수 없습니다. (score: -1)") }
     }
 
     @Test
@@ -18,5 +18,13 @@ class SubjectTests {
 
         assertThrows<IllegalArgumentException>() { subject.changeScore(-1) }
             .apply { assertThat(message).isEqualTo("점수는 음수가 될 수 없습니다. (score : -1)") }
+    }
+
+    @Test
+    fun `점수를 변경한다`() {
+        val subject = Subject(type = SubjectType.Math, score = 12)
+        subject.changeScore(23)
+
+        assertThat(subject.score).isEqualTo(23)
     }
 }

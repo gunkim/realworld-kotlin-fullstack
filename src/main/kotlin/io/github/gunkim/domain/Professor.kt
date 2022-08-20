b358d921306c1e2ac268dd8b3cbccd3f3e208ae4
gunkim <gunkim.dev@gmail.com>
2022-08-20 16:37:29 +0900

    :sparkles: 교수 도메인 테스트 구현

--- src/main/kotlin/io/github/gunkim/domain/Professor.kt
+++ src/main/kotlin/io/github/gunkim/domain/Professor.kt
@@ -3,7 +3,7 @@ package io.github.gunkim.domain
 class Professor(
     val id: Long? = null,
     name: String,
-    students: MutableList<Student>,
+    students: MutableList<Student> = mutableListOf(),
 ) {
     var name: String = name
         private set
@@ -14,5 +14,12 @@ class Professor(
 
     fun removeStudent(student: Student) = require(_students.remove(student)) {
         "'${student.name}' 학생을 찾을 수 없습니다."
+    }
+
+    fun rename(name: String) {
+        require(name.isNotBlank()) {
+            "이름을 공백으로 변경할 수 없습니다."
+        }
+        this.name = name
     }
 }

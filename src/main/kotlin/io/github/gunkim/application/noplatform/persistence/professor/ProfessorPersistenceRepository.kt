9214e193fd8c15bc379fac4aa5e0d5c2c04b38c0
gunkim <gunkim.dev@gmail.com>
2022-09-04 14:39:06 +0900

    :fire: 테스트용 코드 제거

--- src/main/kotlin/io/github/gunkim/application/noplatform/persistence/professor/ProfessorPersistenceRepository.kt
+++ src/main/kotlin/io/github/gunkim/application/noplatform/persistence/professor/ProfessorPersistenceRepository.kt
@@ -3,10 +3,6 @@ package io.github.gunkim.application.noplatform.persistence.professor
 import io.github.gunkim.application.noplatform.persistence.student.*
 import io.github.gunkim.domain.professor.Professor
 import io.github.gunkim.domain.professor.ProfessorRepository
-import io.github.gunkim.domain.student.Student
-import io.github.gunkim.domain.student.Subject
-import io.github.gunkim.domain.student.SubjectType
-import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.SizedIterable
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -31,41 +27,4 @@ class ProfessorPersistenceRepository : ProfessorRepository {
         ProfessorEntity.insertOrUpdate(professor)
             .let { it.toDomain(studentRepository.saveAll(it, professor.students)) }
     }
-}
-
-fun main() {
-
-    transaction {
-        SchemaUtils.drop(ProfessorTable, StudentTable, SubjectTable)
-        SchemaUtils.create(ProfessorTable, StudentTable, SubjectTable)
-    }
-    val repository = ProfessorPersistenceRepository()
-
-    repository.save(
-        Professor(
-            name = "헬로우 거니",
-            students = listOf(
-                Student(
-                    name = "김쿠릉",
-                    subjects = listOf(
-                        Subject(
-                            type = SubjectType.English,
-                            score = 98
-                        ),
-                        Subject(
-                            type = SubjectType.Math,
-                            score = 12
-                        ),
-                        Subject(
-                            type = SubjectType.History,
-                            score = 56
-                        )
-                    )
-                ),
-                Student(
-                    name = "주니"
-                )
-            )
-        )
-    )
 }

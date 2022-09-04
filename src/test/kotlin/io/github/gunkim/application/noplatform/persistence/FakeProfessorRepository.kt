2e2fb612785f61519507733dd2b7e582bc8b9889
gunkim <gunkim.dev@gmail.com>
2022-09-04 19:16:42 +0900

    :recycle: Fake repository 구현 수정

--- src/test/kotlin/io/github/gunkim/application/noplatform/persistence/FakeProfessorRepository.kt
+++ src/test/kotlin/io/github/gunkim/application/noplatform/persistence/FakeProfessorRepository.kt
@@ -8,7 +8,13 @@ class FakeProfessorRepository : ProfessorRepository {
 
     override fun findById(id: Long): Professor = map[id] ?: throw NoSuchElementException("'${id}' 아이디를 가진 교수를 찾을 수 없습니다.")
 
-    override fun save(professor: Professor): Professor = map.set(generateId(), professor).let { professor }
+    override fun save(professor: Professor): Professor {
+        val professorId = professor.id
+            ?.also { map.remove(it) }
+            ?: generateId()
+
+        return map.set(professorId, professor).let { professor }
+    }
 
     private fun generateId(): Long = map.size.toLong() + 1
 }
